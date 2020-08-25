d646cb91ae53e9a49cdc444c60953f6c341085e5
Adrian-Revilla <revilla.adrian02@gmail.com>
2020-08-24 21:22:45 -0300

    .

--- src/PrimerLanding/index.ts
+++ src/PrimerLanding/index.ts
@@ -1,8 +1,15 @@
 require('./index.scss');
+
 require('./img/ejemplo.png');
+require('./img/github.png');
+require('./img/home.png');
 
 //TODO: MEJORAR FUERTEMENTE EL CSS.
 let ArrowToUP = document.querySelector(".scroller") as HTMLDivElement;
+let NavMenu = document.querySelector(".menu") as HTMLDivElement;
+let MenuContent = document.querySelector('.menu_content') as HTMLUListElement;
+
+
 let deslizamiento: number;
 
 
@@ -19,5 +26,23 @@ document.addEventListener("scroll", (e) => {
         ArrowToUP.style.cursor="initial";
     }
 });
+ArrowToUP.addEventListener("click", () => document.documentElement.scrollTop = 0);
 
-ArrowToUP.addEventListener("click",()=>document.documentElement.scrollTop=0);
+NavMenu.addEventListener('click', (e : Event) => {
+    let Anchor = e.target as HTMLAnchorElement;
+
+    if (Anchor.id === 'menu_trigger' && window.innerWidth <= 575 ) {
+        
+
+        if (getComputedStyle(MenuContent).height == '0px') {
+            MenuContent.style.height = `${MenuContent.scrollHeight.toString()}px`
+        } else {
+            MenuContent.style.height = '0px'
+        }
+        
+        
+    }
+    
+})
+
+
